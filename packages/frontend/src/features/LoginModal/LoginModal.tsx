ac48cd3eb5665a46a1e815f8665246f18f91c65d
Aeronnax <aeronnaxog@gmail.com>
2024-09-27 18:06:59 +0500

    Добавлена библиотека zod zod-formik-adapter. Добавлена валидация для  пароля

--- packages/frontend/src/features/LoginModal/LoginModal.tsx
+++ packages/frontend/src/features/LoginModal/LoginModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useFormik } from 'formik';
 import Style from './LoginModal.module.scss';
 
 interface LoginModalProps {
@@ -6,41 +7,87 @@ interface LoginModalProps {
 }
 
 const LoginModal: FC<LoginModalProps> = ({ closeModal }) => {
+  const validate = (values: { email: string; password: string }) => {
+    const errors: { email?: string; password?: string } = {};
+    if (!values.email) {
+      errors.email = 'Поле обязательно для заполнения';
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      errors.email = 'Неверный формат email';
+    }
+    switch (true) {
+      case !values.password:
+        errors.password = 'Поле обязательно для заполнения';
+        break;
+
+      case values.password.length < 6:
+        errors.password = 'Минимальная длина пароля — 6 символов';
+        break;
+
+      case !/[A-Z]/.test(values.password):
+        errors.password =
+          'Пароль должен содержать хотя бы одну заглавную букву';
+        break;
+
+      case !/[!@#$%^&*]/.test(values.password):
+        errors.password = 'Пароль должен содержать хотя бы один спецсимвол';
+        break;
+
+      default:
+        break;
+    }
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log('Вход:', values);
+      closeModal();
+    },
+  });
+
   const stopPropagation = (e: React.MouseEvent): void => {
     e.stopPropagation();
   };
-  // const [login, setLogin] = useState('');
-  // console.log(login);
+
   return (
     <div className={Style.modalOverlay} onClick={closeModal}>
       <div className={Style.modal} onClick={stopPropagation}>
         <h2>Вход</h2>
-        <form className={Style.loginForm}>
+        <form className={Style.loginForm} onSubmit={formik.handleSubmit}>
           <input
             type="email"
+            name="email"
             autoComplete="email"
-            // value={login}
-            // onChange={(e) => {
-            //   const value = e.currentTarget.value;
-            //   if (Number.isNaN(+value)) {
-            //     return;
-            //   }
-            //   setLogin(value);
-            // }}
             placeholder="Введите почту"
             className={Style.inputField}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.email && formik.errors.email && (
+            <div className={Style.errorMessage}>{formik.errors.email}</div>
+          )}
+
           <input
             type="password"
+            name="password"
             autoComplete="current-password"
             placeholder="Введите пароль"
             className={Style.inputField}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
-          <button
-            type="button"
-            className={Style.submitButton}
-            onClick={closeModal}
-          >
+          {formik.touched.password && formik.errors.password && (
+            <div className={Style.errorMessage}>{formik.errors.password}</div>
+          )}
+
+          <button type="submit" className={Style.submitButton}>
             Войти
           </button>
         </form>
