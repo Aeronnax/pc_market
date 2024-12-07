b46b2d3c30d16f76fb296a8cd17e8266490826b4
Aeronnax <aeronnaxog@gmail.com>
2024-12-07 19:45:39 +0500

    Вернул старый таймаут

--- packages/frontend/src/shared/helpers/addTimeout.ts
+++ packages/frontend/src/shared/helpers/addTimeout.ts
@@ -1,24 +1,10 @@
-// export const addTimeout = <TData, TPromise extends Promise<TData>>(
-//   data: TPromise,
-//   ms = 1000
-// ): Promise<TData> => {
-//   return new Promise<TData>((resolve) => {
-//     data.then((response) => {
-//       setTimeout(() => resolve(response), ms);
-//     });
-//   });
-// };
-
-export const addTimeout = <TData>(
-  data: Promise<TData>,
+export const addTimeout = <TData, TPromise extends Promise<TData>>(
+  data: TPromise,
   ms = 1000
 ): Promise<TData> => {
-  return Promise.race([
-    new Promise<TData>((resolve) => setTimeout(() => resolve(data), ms)).then(
-      (result) => result
-    ),
-    new Promise<never>((_, reject) =>
-      setTimeout(() => reject(new Error(`Истек таймаут ${ms} мс`)), ms)
-    ),
-  ]);
+  return new Promise<TData>((resolve) => {
+    data.then((response) => {
+      setTimeout(() => resolve(response), ms);
+    });
+  });
 };
