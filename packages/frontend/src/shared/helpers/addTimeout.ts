e521dec97c7a798f9f3cb4e7ec47f80fd5c6c591
Aeronnax <aeronnaxog@gmail.com>
2024-12-07 19:32:49 +0500

    Фикс таймаута

--- packages/frontend/src/shared/helpers/addTimeout.ts
+++ packages/frontend/src/shared/helpers/addTimeout.ts
@@ -1,20 +1,24 @@
-export const addTimeout = <TData, TPromise extends Promise<TData>>(
-  data: TPromise,
-  ms = 1000
-): TPromise => {
-  return new Promise<TData>((resolve, reject) => {
-    const timer = setTimeout(() => {
-      reject(new Error(`Истек таймаут ${ms}`));
-    }, ms);
+// export const addTimeout = <TData, TPromise extends Promise<TData>>(
+//   data: TPromise,
+//   ms = 1000
+// ): Promise<TData> => {
+//   return new Promise<TData>((resolve) => {
+//     data.then((response) => {
+//       setTimeout(() => resolve(response), ms);
+//     });
+//   });
+// };
 
-    data
-      .then((result) => {
-        clearTimeout(timer);
-        resolve(result);
-      })
-      .catch((error) => {
-        clearTimeout(timer);
-        reject(error);
-      });
-  }) as TPromise;
+export const addTimeout = <TData>(
+  data: Promise<TData>,
+  ms = 1000
+): Promise<TData> => {
+  return Promise.race([
+    new Promise<TData>((resolve) => setTimeout(() => resolve(data), ms)).then(
+      (result) => result
+    ),
+    new Promise<never>((_, reject) =>
+      setTimeout(() => reject(new Error(`Истек таймаут ${ms} мс`)), ms)
+    ),
+  ]);
 };
